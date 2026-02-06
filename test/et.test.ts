ece358b8fd747343daec546a8f22ece8101446ed
dojyorin <ota_droid@live.jp>
2026-02-07 07:42:53 +0900

    fix.

--- test/et.test.ts
+++ test/et.test.ts
@@ -1,27 +1,20 @@
-import {assertEquals} from "jsr:@std/assert@1.0.14";
+import {assertEquals} from "@std/assert";
+import {eorzeatime} from "../src/et.ts";
 
-import {eorzeatime} from "../src/eorzeatime.ts";
-
-Deno.test({
-    name: "Calculate at specified time",
-    fn() {
-        assertEquals(eorzeatime(1750000000), {
-            epoch: 36000000000,
-            year: 1086,
-            month: 1,
-            date: 27,
-            hours: 16,
-            minutes: 0,
-            seconds: 0,
-            monthState: "Astral",
-            monthWithState: 1
-        });
-    }
+Deno.test("Calculate at specified time", () => {
+    assertEquals(eorzeatime(1750000000), {
+        epoch: 36000000000,
+        year: 1086,
+        month: 1,
+        date: 27,
+        hours: 16,
+        minutes: 0,
+        seconds: 0,
+        monthState: "Astral",
+        monthWithState: 1
+    });
 });
 
-Deno.test({
-    name: "Calculate at current time",
-    fn() {
-        eorzeatime();
-    }
+Deno.test("Calculate at current time", () => {
+    eorzeatime();
 });
